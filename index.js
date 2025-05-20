82cd65ed65aa4a3815f6ae2903e010e310aafa5d
SeanR-ScanR <scanr.seanr@gmail.com>
2025-05-20 20:36:21 +0200

    SuwaComicKToImgChestToCubariGist

--- index.js
+++ index.js
@@ -1,64 +1,113 @@
-const fs = require("fs");
-const { openAsBlob } = require("node:fs");
-const dotenv = require("dotenv");
-let _ = require("lodash");
-const { title } = require("process");
+import fs from "fs";
+import dotenv from "dotenv";
+import axios from "axios";
+import _ from "lodash";
+import FormData from "form-data";
+import axiosRateLimit from "axios-rate-limit";
 
+global.FormData = FormData;
 dotenv.config();
 
 const comickFolder =
   "C:/Users/lucas/OneDrive/Documents/Suwayomi/downloads/mangas/Comick (FR)/";
 
-const serie = "TEST";
+const serie = "RuriDragon";
 
 const postRoute = "https://api.imgchest.com/v1/post";
 
-const addPost = (id) => `https://api.imgchest.com/v1/post/${id}/add`;
+const addPostRoute = (id) => `https://api.imgchest.com/v1/post/${id}/add`;
+
+const http = axiosRateLimit(axios.create(), {
+  maxRequests: 30,
+  perMilliseconds: 60000,
+});
 
 const main = async () => {
+  const json = JSON.parse(
+    fs.readFileSync("./template.json", { encoding: "UTF-8" })
+  );
   const mainFolderPath = `${comickFolder}${serie}`;
   const mainFolder = fs.readdirSync(mainFolderPath);
-  const allChapter = mainFolder.forEach(async (folder) => {
+  const promise = mainFolder.map(async (folder) => {
     const regex = folder.match(
-      "([^_]+)_(Vol. ([0-9]+), )?(Ch.|Chapter) ([0-9.]+)_(.*)"
+      "([^_]+)_(Vol. (\\d+), )?(Ch.|Chapter) ([0-9\\.]+)(_(.*))?"
     );
     const chapitre = {
       folder: `${mainFolderPath}/${folder}`,
-      name: regex[6].trim(),
+      name: regex[7]?.trim() ?? regex[5],
       chapitre: regex[5],
       volume: regex[3],
       teams: regex[1],
     };
 
     const chapterFolder = fs.readdirSync(chapitre.folder);
-    const allImage = chapterFolder
-      .filter((imageName) => !imageName.endsWith(".xml"))
-      .map((imageName) => ({
-        fullPath: `${chapitre.folder}/${imageName}`,
-        filename: imageName,
+    const allImage = fs
+      .readdirSync(chapitre.folder)
+      .filter((n) => !n.endsWith(".xml"))
+      .map((n) => ({
+        stream: fs.createReadStream(`${chapitre.folder}/${n}`),
+        filename: n,
       }));
 
-    const chunks = _.chunk(allImage, 20);
+    const chuncks = _.chunk(allImage, 20);
 
-    const batch = chunks.shift();
-
+    const batch = chuncks.shift();
     const form = new FormData();
-    form.append("title", `${title} Chap ${chapitre.chapitre} ${chapitre.name}`);
-    form.append("nsfw", true);
+    form.append("title", `${serie} Chap ${chapitre.chapitre} ${chapitre.name}`);
+    form.append("nsfw", "true");
+    batch.forEach(({ stream, filename }) => {
+      form.append("images[]", stream, filename);
+    });
 
-    for (const img of batch) {
-      form.append("images[]", await openAsBlob(img.fullPath), img.filename);
-    }
-
-    const response = await fetch(postRoute, {
-      method: "POST",
-      body: JSON.stringify(form),
+    const response = await http.post(postRoute, form, {
       headers: {
+        ...form.getHeaders(),
         Authorization: `Bearer ${process.env.IMG_CHEST_TOKEN}`,
       },
+      maxContentLength: Infinity,
+      maxBodyLength: Infinity,
     });
-    console.log(response);
+
+    const jsonPost = response.data;
+    console.log(
+      `\n==============================================================================\nCREATE POST ${serie} ${chapitre.chapitre}`
+    );
+    console.log(response.status, response.data);
+
+    const id = jsonPost.data.id;
+    for (let chunk of chuncks) {
+      const addImageForm = new FormData();
+      chunk.forEach(({ stream, filename }) => {
+        addImageForm.append("images[]", stream, filename);
+      });
+
+      const addImageResponse = await http.post(addPostRoute(id), addImageForm, {
+        headers: {
+          ...addImageForm.getHeaders(),
+          Authorization: `Bearer ${process.env.IMG_CHEST_TOKEN}`,
+        },
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+      });
+      console.log(
+        `\n-----------------------------------------------------------------------------\nADD IMAGE ${serie} ${chapitre.chapitre}`
+      );
+      console.log(addImageResponse.status, addImageResponse.data);
+    }
+    json.chapters[chapitre.chapitre] = {
+      title: chapitre.name,
+      volume: chapitre?.volume ?? "",
+      last_updated: Date.now(),
+      groups: {
+        [chapitre.teams]: `/proxy/api/imgchest/chapter/${id}`,
+      },
+    };
   });
+  await Promise.all(promise);
+  fs.writeFileSync(
+    `./${serie.replaceAll(/\s/g, "_")}.json`,
+    JSON.stringify(json, null, "\t")
+  );
 };
 
 main();
